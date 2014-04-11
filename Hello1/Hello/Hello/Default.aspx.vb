b0dd8a3b0e190a26c03420723aec6c214424d2bf
charles1024 <charlesw1024@gmail.com>
2014-04-11 12:26:12 -0500

    Example code
    This is the sample code that I have used to reproduce the crossover
    issue.

---
+++ Hello1/Hello/Hello/Default.aspx.vb
@@ -0,0 +1,7 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        lblHelloWorld.Text = "Hello World!"
+    End Sub
+End Class
---
+++ Hello1/Hello/Hello/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Hello.Global_asax" Language="vb" %>
